a668bea9cf5b0a6f7a61de1afd1a32b2f0f78771
farzanamona <farzana2mona@gmail.com>
2019-04-05 02:21:21 +0600

    This is my first Commit.

---
+++ app/src/main/java/com/example/atozcom/PostSellProductActivity.java
@@ -0,0 +1,13 @@
+package com.example.atozcom;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PostSellProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_sell_product);
+    }
+}
